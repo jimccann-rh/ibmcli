3f2403a8ac48e4a63138184778b18a7f5bab1911
jimb0 <jimccann@redhat.com>
2021-11-10 14:28:59 -0500

    feat: add local .ssh files in container during runtime

--- runit.sh
+++ runit.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+SETPODSSHKEYS=$HOME/.ssh:/run/localsshkeys
+
+if grep -qF "$SETPODSSHKEYS" /etc/containers/mounts.conf ; then
+   echo "Local SSH key path for podman is set. Look under /run/localsshkeys/"
+else
+   echo $SETPODSSHKEYS | sudo tee -a /etc/containers/mounts.conf /dev/null
+fi
+
 podman run --env-file env.list --rm --name IBMCLI -it ibmcli $@
