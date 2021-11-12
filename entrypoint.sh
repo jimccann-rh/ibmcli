c38edfda9d0fe76c3399ead35505c3a1bf1d08d1
jimb0 <jimccann@redhat.com>
2021-11-12 15:26:49 -0500

    refactor: vars

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $INTERATCIBMCLI = false ] ; then
-/usr/local/bin/ibmcloud login --apikey $IC_API_KEY --no-region > /tmp/login.txt
+/usr/local/bin/ibmcloud login --apikey $IBMCLOUD_API_KEY --no-region > /tmp/login.txt
 /usr/local/bin/ibmcloud $@
 else
 bash
