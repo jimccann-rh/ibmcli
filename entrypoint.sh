0b19389de89cda48a04f073fdb0e24eb4113216e
jimb0 <jimccann@redhat.com>
2021-11-12 16:10:04 -0500

    feat: 1 var for 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export IC_API_KEY=${IBMCLOUD_API_KEY}
+
 if [ $INTERATCIBMCLI = false ] ; then
 /usr/local/bin/ibmcloud login --apikey $IBMCLOUD_API_KEY --no-region > /tmp/login.txt
 /usr/local/bin/ibmcloud $@
